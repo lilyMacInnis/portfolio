f9a71c87595cb53ab782af13103a1e36482bb43a
ceilingFan25 <lilymacinnis@gmail.com>
2024-09-28 23:26:07 -0400

    Add files via upload

---
+++ simple_project.js
@@ -0,0 +1,10 @@
+
+function dropDown(){
+    var x = document.getElementById("header");
+    if(x.style.display === "block"){
+        x.style.display = "none";
+    }
+    else{
+        x.style.display = "block";
+    }
+}
